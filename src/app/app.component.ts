c95a84539add1901e068eb34b1afa57c66da7b3d
SpDim <ioannina.spyros1986@gmail.com>
2023-10-16 18:39:36 +0300

    draft-output-component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,21 +1,113 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { Person } from './interfaces/person';
+
+import { PersonComponent } from './person/person.component';
+import { PersonAltComponent } from './person-alt/person-alt.component';
+import { EventBindComponent } from './event-bind/event-bind.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule],
+  imports: [
+    CommonModule,
+    PersonComponent,
+    PersonAltComponent,
+    EventBindComponent,
+  ],
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  name: string = 'Spyros';
-  lastName = 'Dim';
+  name: string = 'Christodoulos';
+  lastName = 'Fragkoudakis';
 
-  person = {
-    givenName: "Spyros",
-    surnName: "Dim",
-    age: 0x23,
-    email: "demo@gmail.com",    
-  }
+  person: Person = {
+    givenName: 'Christodoulos',
+    surName: 'Fragkoudakis',
+    age: 0x37,
+    email: 'chfrag@aueb.gr',
+    address: 'Athens, Greece',
+  };
+
+  person2: Person = {
+    givenName: 'Giorgos',
+    surName: 'Chatzidimitrakopoulos',
+    age: 22,
+    email: 'chatzi@aueb.gr',
+    address: 'Athens, Greece',
+  };
+
+  users: Person[] = [
+    {
+      givenName: 'John',
+      surName: 'Doe',
+      age: 30,
+      email: 'john.doe@example.com',
+      address: '123 Main St',
+    },
+    {
+      givenName: 'Jane',
+      surName: 'Doe',
+      age: 28,
+      email: 'jane.doe@example.com',
+      address: '123 Main St',
+    },
+    {
+      givenName: 'Jim',
+      surName: 'Brown',
+      age: 45,
+      email: 'jim.brown@example.com',
+      address: '456 Park Ave',
+    },
+    {
+      givenName: 'Jill',
+      surName: 'Brown',
+      age: 42,
+      email: 'jill.brown@example.com',
+      address: '456 Park Ave',
+    },
+    {
+      givenName: 'Jake',
+      surName: 'Smith',
+      age: 36,
+      email: 'jake.smith@example.com',
+      address: '789 Broadway',
+    },
+    {
+      givenName: 'Judy',
+      surName: 'Smith',
+      age: 34,
+      email: 'judy.smith@example.com',
+      address: '789 Broadway',
+    },
+    {
+      givenName: 'Jack',
+      surName: 'Johnson',
+      age: 50,
+      email: 'jack.johnson@example.com',
+      address: '321 Oak St',
+    },
+    {
+      givenName: 'Julie',
+      surName: 'Johnson',
+      age: 48,
+      email: 'julie.johnson@example.com',
+      address: '321 Oak St',
+    },
+    {
+      givenName: 'Jerry',
+      surName: 'Davis',
+      age: 55,
+      email: 'jerry.davis@example.com',
+      address: '654 Pine St',
+    },
+    {
+      givenName: 'June',
+      surName: 'Davis',
+      age: 53,
+      email: 'june.davis@example.com',
+      address: '654 Pine St',
+    },
+  ];
 }
---
+++ src/app/person/person.component.ts
@@ -0,0 +1,20 @@
+import { Component, Input } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Person } from '../interfaces/person';
+
+@Component({
+  selector: 'app-person',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './person.component.html',
+  styleUrls: ['./person.component.css'],
+})
+export class PersonComponent {
+  @Input() person: Person = {
+    givenName: "The person's First Name",
+    surName: "The person's Last Name",
+    age: 0,
+    email: "The person's email",
+    address: "The person's Address",
+  };
+}
