724c7055e1bde65848afe5e5e940a6eeb9b9ea78
SpDim <ioannina.spyros1986@gmail.com>
2023-11-06 23:26:14 +0200

    delete user by id

--- src/app/crud-demo/delete-user/delete-user.component.ts
+++ src/app/crud-demo/delete-user/delete-user.component.ts
@@ -1,5 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, EventEmitter, Inject, Output, ViewChild } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { Person } from 'src/app/interfaces/person';
+import { AppService } from 'src/app/app.service';
 
 @Component({
   selector: 'app-delete-user',
@@ -9,5 +11,29 @@ import { CommonModule } from '@angular/common';
   styleUrls: ['./delete-user.component.css']
 })
 export class DeleteUserComponent {
+  @Output() userDeleted = new EventEmitter();
+
+  foundUser: Person | undefined;
+  userNotFound: boolean = false;
+
+  @ViewChild('userId') userIdInput!: ElementRef<HTMLInputElement>;
+
+  constructor(private appService: AppService = Inject(AppService)) {}
+
+  onClick() {
+    const id = this.userIdInput.nativeElement.value;
+    this.appService.deleteUser(parseInt(id)).subscribe({
+      next: (user) => {
+        console.log('Deleted user:', user);
+        this.userNotFound = false;
+        this.userDeleted.emit();
+      },
+      error: (err) => {
+        console.log('Error:', err)
+        this.userNotFound = true;
+      },
+      complete: () => {console.log('Delete operation completed')}
+    });
+  }
 
 }
